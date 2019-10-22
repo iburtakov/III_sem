35a1471024c4f3af744087a7e30773b2daea3797
iburtakov <burtakov.ia@phystech.edu>
2019-10-23 01:40:21 +0300

    Renew

---
+++ myexec/cmake-build-debug/CMakeFiles/myexec.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myexec.dir/main.cpp.obj"
+  "myexec.pdb"
+  "myexec.exe"
+  "myexec.exe.manifest"
+  "libmyexec.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myexec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
